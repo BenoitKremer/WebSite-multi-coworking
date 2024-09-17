010f90041a305b8f6c663d881dc7212a2a590919
BenoitKremer <benoit.kremer@epitech.eu>
2024-09-17 21:29:06 +0200

    Base API REST + liaison BDD

--- app/server/database.js
+++ app/server/database.js
@@ -1,22 +1,16 @@
 const {Client} = require('pg');
-const {DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_DATABASE} = process.env;
+
+require('dotenv').config();
+
+const { DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME } = process.env;
+
 
 const client = new Client({
     host:DB_HOST,
     user:DB_USER,
     port:DB_PORT,
     password:DB_PASSWORD,
-    database:DB_DATABASE
+    database:DB_NAME
 })
 
-client.connect()
-
-client.query('SELECT * FROM users', (err, res) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(res.rows);
-    client.end();
-})
-
+module.exports = client
--- app/server/server.js
+++ app/server/server.js
@@ -1,10 +1,87 @@
+/* eslint-disable no-unused-expressions */
+const client = require('./database.js');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-app.get('/api', (req, res) => {
-  res.json({ "message": 'Hello from server!' });
-});
 
-app.listen(5000, () => {
-  console.log('Server listening on port 5000');
-});
+app.listen(3300, () => {
+  console.log("Server listening on port 3300");
+})
+
+client.connect();
+
+app.get('/users', (req, res)=>{
+  client.query('Select * from users', (err, result)=>{
+    if(!err){
+      res.send(result.rows);
+    }
+  });
+  client.end;
+})
+
+app.get('/users/:id', (req, res)=>{
+  client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
+    if(!err){
+      res.send(result.rows);
+    }
+  });
+  client.end;
+})
+
+app.post('/users', (req, res)=>{
+  const user = req.body;
+  let query = `insert into users values(
+    ${user.id},
+    '${user.first_name}',
+    '${user.last_name}',
+    '${user.email}',
+    '${user.phone_number}',
+    '${user.password}')`
+
+  client.query(query,  (err, result)=>{
+    if(!err){
+      res.send('User added successfully')
+    }
+    else{
+      console.log(err.message)
+    }
+  })
+  client.end;
+})
+
+app.put('/users/:id', (req, res)=>{
+  let user = req.body;
+  let query = `update users set
+    first_name = '${user.first_name}',
+    last_name = '${user.last_name}',
+    email = '${user.email}',
+    phone_number = '${user.phone_number}',
+    password = '${user.password}'
+    where id=${user.id}`
+
+  client.query(query,  (err, result)=>{
+    if(!err){
+      res.send('User updated successfully')
+    }
+    else{
+      console.log(err.message)
+    }
+  })
+  client.end;
+})
+
+app.delete('/users/:id', (req, res)=>{
+  let query = `delete from users where id=${req.params.id}`
+
+  client.query(query,  (err, result)=>{
+    if(!err){
+      res.send('User deleted successfully')
+    }
+    else{
+      console.log(err.message)
+    }
+  })
+  client.end;
+})
